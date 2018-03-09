6753959f07d2b2d84ddffe06869b87de82d980ca
Scott <scott.tolinski@gmail.com>
2018-03-09 10:23:56 -0700

    adds form

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,15 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <form
+      name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input name="name" placeholder="Your Name" type="text" />
+      <button>Send</button>
+    </form>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
